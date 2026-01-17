1c475d1e7bd8be84ab36e7571085579ecb9a5ab3
SHARAF <252891507+shxrxf-code@users.noreply.github.com>
2026-01-17 16:49:36 +0530

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,5 +1,7 @@
 import { Link, useLocation } from 'react-router-dom';
 import { useState } from 'react';
+import logo from "../assets/logo/Logo.png";
+
 
 export default function Navigation() {
   const location = useLocation();
